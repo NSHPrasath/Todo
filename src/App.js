ccb1fecc1534dfe1f2377ce845708f57ba87c33b
HariPrasath <ns.h.prasath301@gmail.com>
2024-09-30 17:27:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/js/bootstrap.js";
+import "./App.css";
+import Todolist from "./components/Todolist";
+
+function App() {
+  return (
+    <div className="styles">
+      <Todolist />
+    </div>
+  );
+}
+
+export default App;
